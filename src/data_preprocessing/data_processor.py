e08973d11e418bc8466907cb7251259d422a49c0
sztyberj <sztybix@gmail.com>
2025-06-22 12:36:04 +0200

    Refactor: Rebuild data processing classes

--- src/data_preprocessing/data_processor.py
+++ src/data_preprocessing/data_processor.py
@@ -3,139 +3,144 @@ import numpy as np
 import sys
 from pathlib import Path
 from datetime import datetime
+from sklearn.base import BaseEstimator, TransformerMixin
+import toml
+import warnings
 
-current_file = Path(__file__).resolve()
-project_root = current_file.parents[2]
-sys.path.append(str(project_root))
+pd.set_option('display.float_format', lambda x: f'{x:.4f}')
+pd.set_option('future.no_silent_downcasting', True)
+warnings.filterwarnings('ignore')
 
-DATA_DIR = project_root / "data" / "cleaned"
+THIS_FILE = Path(__file__).resolve()
+ROOT_DIR = THIS_FILE.parents[2]
+sys.path.append(str(ROOT_DIR))
 
-from src.eda.data_reader import DataReader
+DATA_DIR = ROOT_DIR / "data" / "cleaned"
+
+with open(ROOT_DIR / "config.toml", 'r') as f:
+    config = toml.load(f)
 
 
-class DataProcessor():
-    def __init__(self, df: pd.DataFrame):
-        self.df = df.copy()
-        
-    def clean_price(self):
-        self.df['price'] = pd.to_numeric(self.df['price'], errors='coerce')
-        self.df = self.df.dropna(subset=['price'])
-    
-        return self
-    
-    def clean_ppm(self):
-        self.df.loc[:,'price_per_meter'] = self.df['price_per_meter'].fillna(self.df['price'] / self.df['area'])
-        self.df['price_per_meter'] = pd.to_numeric(self.df['price_per_meter'], errors='coerce')
+class DataProcessor(BaseEstimator, TransformerMixin):
+    def __init__(self, config = None):
+        self.config = config if config is not None else {}
 
-        return self
+        dp_config = self.config.get('data_processing', {})
 
-    def clean_rooms(self):
-        self.df.loc[:, 'rooms'] = pd.to_numeric(self.df['rooms'], errors='coerce')
-        self.df = self.df.dropna(subset=['rooms'])
+        self.columns_to_check = dp_config.get('columns_to_check', [])
+        self.columns_to_clean = dp_config.get('columns_to_clean', [])
+        self.columns_to_drop = dp_config.get('columns_to_drop', [])
 
-        return self
+        self.lower = dp_config.get('outlier_lower', 0.05)
+        self.upper = dp_config.get('outlier_upper', 0.95)
 
-    def clean_market_type(self):
-        self.df = self.df.dropna(subset=['market_type'])
+    @staticmethod
+    def save_to_csv(df: pd.DataFrame , version: str):
+        print(f"DataProcessor: save to .csv as {version} version")
+        df.to_csv(f"{DATA_DIR}/v{version}_{datetime.today().strftime('%Y_%m_%d')}.csv", sep=";", index=False)
 
-        return self
+    def fit(self, X, y=None):
+        temp_df = X.copy()
 
-    def clean_furnished(self):
-        self.df = self.df.dropna(subset=['furnished'])
+        #fit median year_built
+        if 'year_built' in temp_df.columns:
+            self.year_built_median = temp_df['year_built'].median()
+            print(f"DataProcessor: Learned median for 'year_built'.")
+        else:
+            self.year_built_median = None
 
-        return self
-
-    def clean_district(self):
-        self.df = self.df.dropna(subset=['district'])
-
-        return self
-
-    def clean_building_type(self):
-        self.df['building_type'] = self.df['building_type'].fillna('unknown')
-        
-        return self
-
-    def clean_year_built(self):
-        self.df.loc[self.df['year_built'] < 1300, 'year_built'] = np.nan
-        self.df.loc[:,'year_built'] = self.df['year_built'].fillna(self.df['year_built'].median())
-        
-        return self
-
-    def clean_rent(self):
-        self.df.loc[:,'rent'] = self.df['rent'].fillna(self.df['rent'].median())
-
-        return self
-
-    def clean_finish_status(self):
-        self.df = self.df.dropna(subset=['finish_status'])
-
-        return self
-
-    def clean_ownership(self):
-        self.df = self.df.dropna(subset=['ownership'])
-
-        return self
-
-    def clean_heating(self):
-        self.df.loc[:,'heating'] = self.df['heating'].fillna('unknown')
-
-        return self
-
-    def clean_elevator(self):
-        self.df = self.df.dropna(subset=['elevator'])
-
-        return self
-
-    def clean_outliers(self, column, lower=0.05, upper=0.95):
-        q_low = self.df[column].quantile(lower)
-        q_high = self.df[column].quantile(upper)
-        
-        self.df.loc[:,column] = self.df[column].clip(lower=q_low, upper=q_high)
-
-        return self
-
-    def drop_duplicates(self):
-        self.df = self.df.drop_duplicates(subset=['price', 'url'])
-        self.df = self.df.reset_index(drop=True)
-
-        return self
-
-    def drop_columns(self, columns: [str]):
-        self.df = self.df.drop(columns, axis=1)
+        #fit median rent
+        if 'rent' in temp_df.columns:
+            temp_df['rent'] = pd.to_numeric(temp_df['rent'], errors='coerce')
+            self.rent_median = temp_df['rent'].median()
+            print(f"DataProcessor: Learned median for 'rent'.")
+        else:
+            self.rent_median = None
 
         return self
         
-    def save_to_csv(self, version: str):
-        self.df.to_csv(f"{DATA_DIR}/v{version}_{datetime.today().strftime('%Y_%m_%d')}.csv", sep=";", index=False)
+    def transform(self, X, y=None):
+        df = X.copy()
+        #price
+        if 'price' in df.columns:
+            df['price'] = pd.to_numeric(df['price'], errors='coerce')
+            df = df.dropna(subset=['price'])
+            print("DataProcessor: 'price' transformed.")
 
-        return self
+        #ppm
+        if 'price_per_meter' in df.columns:
+            df.loc[:,'price_per_meter'] = df['price_per_meter'].fillna(df['price'] / df['area'])
+            df['price_per_meter'] = pd.to_numeric(df['price_per_meter'], errors='coerce')
+            print("DataProcessor: 'ppm' transformed.")
+
+        #rooms
+        if 'rooms' in df.columns:
+            df.loc[:, 'rooms'] = pd.to_numeric(df['rooms'], errors='coerce')
+            df = df.dropna(subset=['rooms'])
+            print("DataProcessor: 'rooms' transformed.")
+
+        #building_type
+        if 'building_type' in df.columns:
+            df['building_type'] = df['building_type'].fillna('unknown')
+            print("DataProcessor: 'building_type' filled.")
+
+        #year_built
+        if 'year_built' in df.columns:
+            df.loc[df['year_built'] < 1300, 'year_built'] = np.nan
+            df.loc[:,'year_built'] = df['year_built'].fillna(self.year_built_median)
+            print("DataProcessor: 'year_built' filled.")
+        
+        #rent
+        if 'rent' in df.columns:
+            df.loc[:,'rent'] = df['rent'].fillna(self.rent_median)
+            print("DataProcessor: 'rent' filled.")
+
+        #drop_na
+        existing_columns_to_check = [col for col in self.columns_to_check if col in df.columns]
+        if existing_columns_to_check:
+            df = df.dropna(subset=existing_columns_to_check)
+            print(f"DataProcessor: 'na' i columns: {existing_columns_to_check}  dropped.")
+
+        #heating
+        if 'heating' in df.columns:
+            df.loc[:,'heating'] = df['heating'].fillna('unknown')
+            print("DataProcessor: 'heating' filled.")
+
+        #duplicates
+        required_columns = {'price', 'url'}
+        if required_columns.issubset(df.columns):
+            df = df.drop_duplicates(subset=['price', 'url'])
+            df = df.reset_index(drop=True)
+            print("DataProcessor: duplicates dropped.")
+
+        #clean_outliers
+        existing_columns_to_clean = [col for col in self.columns_to_clean if col in df.columns]
+        if existing_columns_to_clean:
+            for i in existing_columns_to_clean:
+                q_low = df[i].quantile(self.lower)
+                q_high = df[i].quantile(self.upper)
+        
+                df.loc[:,i] = df[i].clip(lower=q_low, upper=q_high)
+                print(f"DataProcessor: {i} outliers cleaned.")
+
+        #drop_columns
+        existing_columns_to_drop = [col for col in self.columns_to_drop if col in df.columns]
+        if existing_columns_to_drop:
+            df = df.drop(columns=existing_columns_to_drop, errors='ignore')
+            print(f"DataProcessor: columns {existing_columns_to_drop} dropped.")
+        
+        return df
 
 
 if __name__ == "__main__":
+    #Run from ./Real Estate Price Prediction/
+    from src.eda.data_reader import DataReader
     reader = DataReader()
     df = reader.read()
 
-    data_processor = (DataProcessor(df)
-      .clean_price()
-      .clean_ppm()
-      .clean_rooms()
-      .clean_market_type()
-      .clean_furnished()
-      .clean_district()
-      .clean_building_type()
-      .clean_year_built()
-      .clean_rent()
-      .clean_finish_status()
-      .clean_ownership()
-      .clean_heating()
-      .clean_elevator()
-      .clean_outliers('price')
-      .clean_outliers('price_per_meter')
-      .clean_outliers('rent')
-      .drop_duplicates()
-      .drop_columns(['source', 'date', 'url', 'title', 'ad_id', 'external_id'])
-      .save_to_csv("T")
-     )
+    data_processor = DataProcessor(config)
+    data_processor.fit(df)
+    df = data_processor.transform(df)
+    data_processor.save_to_csv(df, version="T")
 
-    df = data_processor.df
     print(df)
--- src/data_preprocessing/feature_engineer.py
+++ src/data_preprocessing/feature_engineer.py
@@ -3,158 +3,197 @@ import numpy as np
 import sys
 from pathlib import Path
 from datetime import datetime
-import joblib
-from sklearn.preprocessing import StandardScaler
+from sklearn.base import BaseEstimator, TransformerMixin
+import toml
+import warnings
 
 pd.set_option('display.float_format', lambda x: f'{x:.4f}')
 pd.set_option('future.no_silent_downcasting', True)
+warnings.filterwarnings('ignore')
 
-current_file = Path(__file__).resolve()
-project_root = current_file.parents[2]
-sys.path.append(str(project_root))
+THIS_FILE = Path(__file__).resolve()
+ROOT_DIR = THIS_FILE.parents[2]
+sys.path.append(str(ROOT_DIR))
 
-DATA_DIR = project_root / "data" / "processed"
-PICKLE_DIR = project_root / "data" / "processed" / "pickle"
+DATA_DIR = ROOT_DIR / "data" / "processed"
 
-class FeatureEngineer():
-    def __init__(self, df: pd.DataFrame):
-        self.df = df.copy()
-        self.scalers = {}
+with open(ROOT_DIR / "config.toml", 'r') as f:
+    config = toml.load(f)
+
+class FeatureEngineer(BaseEstimator, TransformerMixin):
+    def __init__(self, config_feature = None):
+        self.config = config_feature if config_feature is not None else {}
+
+        fe_config = self.config.get('feature_engineering', {})
+
+        self.freq_encode_cols = fe_config.get('frequency_encode_cols', [])
         self.encodings = {}
 
+        self.one_hot_encode_cols = fe_config.get('one_hot_encode_cols', [])
+        self.drop_original_onehot = fe_config.get('drop_original_onehot', True)
+        self.categories_ = {}
+        
+        self.luxury_quantile = fe_config.get('luxury_quantile', 0.9)
+        self.price_per_meter_quantile_threshold = None
+
+        self.binary_reduce_cols = fe_config.get('binary_reduce_cols', {})
+
+        self.cols_to_drop_final = fe_config.get('columns_to_drop_final', [])
+
+        self.floor_map = fe_config.get('floor_map', {})
+
+        self.top_building_types = fe_config.get('top_building_types', [])
+
     @staticmethod
-    def load(path: str):
-        print(f"Loading PreProcessor from: {path}")
-        
-        return joblib.load(path)
-
-    def save(self, path: str):
-        joblib.dump(self, path)
-        print(f"PreProcessor saved to: {path}")
-
-    def process_floor(self):
-        #split floor column by sep "/"
-        self.df[['floor', 'building_max_floor']]  = self.df['floor'].str.split('/', expand=True)
-        
-        floor_map = {
-        'suterena': -1,
-        'parter': 0,
-        }
+    def save_to_csv(df: pd.DataFrame , version: str):
+        print(f"FeatureEngineer: save to .csv as {version} version")
+        df.to_csv(f"{DATA_DIR}/v{version}_{datetime.today().strftime('%Y_%m_%d')}.csv", sep=";", index=False)
     
-        self.df['floor'] = self.df['floor'].replace(floor_map)
-    
-        self.df['building_max_floor'] = pd.to_numeric(self.df['building_max_floor'], errors='coerce')
-        self.df.loc[df['floor'] == 'poddasze', 'floor'] = self.df['building_max_floor'] + 1
-            
-        self.df['is_above_10_floor'] = self.df['floor'].astype(str).str.contains('>').astype(int)
-    
-        self.df.loc[:,'floor'] = self.df['floor'].astype(str).str.replace('>', '', regex=False)
-    
-        self.df['floor'] = pd.to_numeric(self.df['floor'], errors='coerce')
-
-        self.df.loc[self.df['building_max_floor'] > 60, 'building_max_floor'] = np.nan
+    def fit(self, X: pd.DataFrame, y=None):
+        temp_X_for_fit = X.copy()
         
-        self.df = self.df.dropna(subset=['floor', 'building_max_floor'])
-    
-        return self
+        #Preprocessing
+        if 'building_type' in self.one_hot_encode_cols and 'building_type' in temp_X_for_fit.columns:
+            temp_X_for_fit['building_type'] = temp_X_for_fit['building_type'].apply(
+                lambda x: x if x in self.top_building_types else 'other'
+            )
+            print("FeatureEngineer: 'building_type' reduced for fit purposes (to learn correct OHE categories).")
 
-    def has_elevator_in_desc(self):
-        has_elevator = self.df['description'].str.contains(r'\bwinda\w*\b', case=False, na=False)
-        self.df.loc[has_elevator, 'elevator'] = 1
+        #Frequency encoding
+        for col in self.freq_encode_cols:
+            if col in X.columns:
+                freq_map = X[col].value_counts(normalize=True).to_dict()
+                self.encodings[col] = freq_map
+                print(f"FeatureEngineer: Learned frequency map for '{col}'.")
 
-        self.df['elevator'] = self.df['elevator'].replace({'Tak': 1, 'Nie': 0})
-        self.df['elevator'] = self.df['elevator'].fillna(0).astype(int)
-    
-        return self
+        #One hot encoding
+        for col in self.one_hot_encode_cols:
+            if col in temp_X_for_fit.columns:
+                self.categories_[col] = pd.Categorical(temp_X_for_fit[col]).categories.tolist()
+                print(f"FeatureEngineer: Learned categories for '{col}': {self.categories_[col]}.")
 
-    def has_balcony_in_desc(self):
-        has_balcony = self.df['description'].str.contains(r'\b(balkon\w*|taras\w*)\b', case=False, na=False)
-        self.df.loc[has_balcony, 'balcony'] = 1
-
-        self.df['balcony'] = self.df['balcony'].fillna(0)
-    
-        return self
-
-
-    def has_garage_in_desc(self):
-        has_garage = self.df['description'].str.contains(r'\bgaraż\w*\b', case=False, na=False)
-        self.df.loc[has_garage, 'garage'] = 1
-        self.df['garage'] = self.df['garage'].fillna(0)
-    
-        return self
-
-    def has_furniture_in_desc(self):
-        has_furniture = self.df['description'].str.contains( r'\bmeble\w*\b', case=False, na=False)
-        self.df.loc[has_furniture, 'furnished'] = 1
-
-        self.df['furnished'] = self.df['furnished'].replace({'Tak': 1, 'Nie': 0})
-        self.df['furnished'] = self.df['furnished'].fillna(0).astype(int)
-    
-        return self
-        
-    def create_luxury_col(self):
-        self.df["is_luxury"] = (self.df["price_per_meter"] > self.df["price_per_meter"].quantile(0.90)).astype(int)
+        #Is_luxury threshold
+        if 'price_per_meter' in X.columns:
+            self.price_per_meter_quantile_threshold = X['price_per_meter'].quantile(self.luxury_quantile)
+            print(f"FeatureEngineer: Learned luxury threshold for 'price_per_meter': {self.price_per_meter_quantile_threshold:.2f}")
+        else:
+            self.price_per_meter_quantile_threshold = 0
 
         return self
 
-    def create_room_per_area_col(self):
-        self.df["rooms_per_area"] = (self.df["rooms"] / self.df["area"]).astype(float)
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        df_transformed = X.copy()
 
-        return self
+        #--- static columns
+        #1. process floor
+        if 'floor' in df_transformed.columns:
+            if not df_transformed['floor'].isnull().all():
+                df_transformed[['floor', 'building_max_floor']] = df_transformed['floor'].astype(str).str.split('/', expand=True)
 
-    def reduce_to_binary(self, column: str, positive_value: str, new_name: str = None):
-        if new_name is None:
-            new_name = column
-        self.df[new_name] = (self.df[column] == positive_value).astype(int)
-        
-        return self
+            df_transformed['floor'] = df_transformed['floor'].replace(self.floor_map)
+            df_transformed['building_max_floor'] = pd.to_numeric(df_transformed['building_max_floor'], errors='coerce')
 
-    def frequency_encoding(self, column: str):
-        freq_map = self.df[column].value_counts(normalize=True)
+            df_transformed.loc[df_transformed['floor'] == 'poddasze', 'floor'] = df_transformed['building_max_floor'] + 1
 
-        self.encodings[column] = freq_map.to_dict()
-        
-        self.df[f'{column}_freq'] = self.df[column].map(freq_map)
-        
-        return self
+            df_transformed['is_above_10_floor'] = df_transformed['floor'].astype(str).str.contains('>').astype(int)
 
-    def one_hot_encode(self, column: str, drop_original: bool = True):
-        dummies = pd.get_dummies(self.df[column], prefix=column, drop_first=True, dtype=int)
-        self.df = pd.concat([self.df, dummies], axis=1)
-        if drop_original:
-            self.df.drop(columns=[column], inplace=True)
-        return self
+            df_transformed['floor'] = df_transformed['floor'].astype(str).str.replace('>', '', regex=False)
+            df_transformed['floor'] = pd.to_numeric(df_transformed['floor'], errors='coerce')
 
-    def reduce_building_type(self):
-        top_types = {'apartment', 'block', 'tenement'}
-        self.df['building_type'] = self.df['building_type'].apply(
-            lambda x: x if x in top_types else 'other'
-        )
-        return self
+            df_transformed.loc[df_transformed['building_max_floor'] > 60, 'building_max_floor'] = np.nan
 
-    def drop_columns(self, columns: [str]):
-        self.df = self.df.drop(columns, axis=1)
-        self.df = self.df.reset_index(drop=True)
-        
-        return self
+            if 'floor' in df_transformed.columns and 'building_max_floor' in df_transformed.columns:
+                df_transformed = df_transformed.dropna(subset=['floor', 'building_max_floor'])
 
-    def scale_columns(self, columns: list, method='standard'):
-        for col in columns:
-            if col not in self.df.columns:
-                print(f"[SKIP] Column '{col}' not found.")
-                continue
+            print("FeatureEngineer: 'floor' and 'building_max_floor' processed.")
 
-            scaler = StandardScaler()
-            self.df[col] = scaler.fit_transform(self.df[[col]])
-            self.scalers[col] = scaler
+        #2. has_elevator_in_desc
+        if 'description' in df_transformed.columns:
+            has_elevator = df_transformed['description'].str.contains(r'\bwinda\w*\b', case=False, na=False)
+            df_transformed.loc[has_elevator, 'elevator'] = 1
+            df_transformed['elevator'] = df_transformed['elevator'].replace({'Tak': 1, 'Nie': 0})
+            df_transformed['elevator'] = df_transformed['elevator'].fillna(0).astype(int)
+            print("FeatureEngineer: 'elevator' feature created.")
 
-        print(f"Scaled columns: {columns}")
-        return self
-        
-    def save_to_csv(self, version: str):
-        self.df.to_csv(f"{DATA_DIR}/v{version}_{datetime.today().strftime('%Y_%m_%d')}.csv", sep=";", index=False)
+        #3. has_balcony_in_desc
+        if 'description' in df_transformed.columns:
+            has_balcony = df_transformed['description'].str.contains(r'\b(balkon\w*|taras\w*)\b', case=False, na=False)
+            df_transformed.loc[has_balcony, 'balcony'] = 1
+            df_transformed['balcony'] = df_transformed['balcony'].fillna(0).astype(int) # Change to int
+            print("FeatureEngineer: 'balcony' feature created.")
 
-        return self
+        # 4. has_garage_in_desc
+        if 'description' in df_transformed.columns:
+            has_garage = df_transformed['description'].str.contains(r'\bgaraż\w*\b', case=False, na=False)
+            df_transformed.loc[has_garage, 'garage'] = 1
+            df_transformed['garage'] = df_transformed['garage'].fillna(0).astype(int) # Change to int
+            print("FeatureEngineer: 'garage' feature created.")
+
+        # 5. has_furniture_in_desc
+        if 'description' in df_transformed.columns:
+            has_furniture = df_transformed['description'].str.contains(r'\bmeble\w*\b', case=False, na=False)
+            df_transformed.loc[has_furniture, 'furnished'] = 1
+            df_transformed['furnished'] = df_transformed['furnished'].replace({'Tak': 1, 'Nie': 0})
+            df_transformed['furnished'] = df_transformed['furnished'].fillna(0).astype(int)
+            print("FeatureEngineer: 'furnished' feature created.")
+
+        # 6. create room_per_area col
+        if "rooms" in df_transformed.columns and "area" in df_transformed.columns and not df_transformed['area'].eq(0).any():
+            df_transformed["rooms_per_area"] = (df_transformed["rooms"] / df_transformed["area"]).astype(float)
+            # Handle potential division by zero results
+            df_transformed['rooms_per_area'].replace([np.inf, -np.inf], np.nan, inplace=True)
+            df_transformed['rooms_per_area'].fillna(0, inplace=True) # Fill with 0 or median/mean
+            print("FeatureEngineer: 'rooms_per_area' feature created.")
+
+        # 7. reduce_to_binary
+        for col, positive_val in self.binary_reduce_cols.items():
+            if col in df_transformed.columns:
+                df_transformed[col] = (df_transformed[col] == positive_val).astype(int)
+                print(f"FeatureEngineer: Reduced '{col}' to binary.")
+
+        # 8. reduce_building_type
+        if 'building_type' in df_transformed.columns:
+            top_types = self.top_building_types
+            df_transformed['building_type'] = df_transformed['building_type'].apply(
+                lambda x: x if x in top_types else 'other'
+            )
+            print("FeatureEngineer: 'building_type' reduced.")
+
+        #required 'fit'
+        # 9. create_luxury_col (wymaga price_per_meter i nauczonego progu)
+        if "price_per_meter" in df_transformed.columns and self.price_per_meter_quantile_threshold is not None:
+            df_transformed["is_luxury"] = (df_transformed["price_per_meter"] > self.price_per_meter_quantile_threshold).astype(int)
+            print("FeatureEngineer: 'is_luxury' feature created.")
+
+        #10. frequency_encoding
+        for col in self.freq_encode_cols:
+            if col in df_transformed.columns and col in self.encodings:
+                df_transformed[f'{col}_freq'] = df_transformed[col].map(self.encodings[col]).fillna(0)
+                print(f"FeatureEngineer: Applied frequency encoding for '{col}'.")
+
+        #11. one_hot_encoding
+        for col in self.one_hot_encode_cols:
+            if col in df_transformed.columns and col in self.categories_:
+                df_transformed[col] = pd.Categorical(df_transformed[col], categories=self.categories_[col])
+                
+                dummies = pd.get_dummies(df_transformed[col], prefix=col, 
+                                         drop_first=self.drop_original_onehot, dtype=int)
+                df_transformed = pd.concat([df_transformed, dummies], axis=1)
+                
+                if self.drop_original_onehot:
+                    df_transformed.drop(columns=[col], inplace=True)
+                print(f"FeatureEngineer: Applied One-Hot Encoding for '{col}'.")
+
+        #12. drop columns
+        cols_to_actually_drop = [c for c in self.cols_to_drop_final if c in df_transformed.columns]
+        if cols_to_actually_drop:
+            df_transformed = df_transformed.drop(cols_to_actually_drop, axis=1)
+            print(f"FeatureEngineer: Dropped columns: {cols_to_actually_drop}")
+
+        df_transformed = df_transformed.reset_index(drop=True)
+
+        print("FeatureEngineer: Transformation complete.")
+        return df_transformed
 
 if __name__ == "__main__":
     from src.eda.data_reader import DataReader
@@ -163,52 +202,15 @@ if __name__ == "__main__":
     reader = DataReader()
     df = reader.read()
     
-    data_processor = (DataProcessor(df)
-      .clean_price()
-      .clean_ppm()
-      .clean_rooms()
-      .clean_market_type()
-      .clean_furnished()
-      .clean_district()
-      .clean_building_type()
-      .clean_year_built()
-      .clean_rent()
-      .clean_finish_status()
-      .clean_ownership()
-      .clean_heating()
-      .clean_elevator()
-      .clean_outliers('price')
-      .clean_outliers('price_per_meter')
-      .clean_outliers('rent')
-      .drop_duplicates()
-      .drop_columns(['source', 'date', 'url', 'title', 'ad_id', 'external_id'])
-      .save_to_csv("T")
-     )
+    data_processor = DataProcessor(config)
+    data_processor.fit(df)
+    df = data_processor.transform(df)
+    data_processor.save_to_csv(df, version="T")
 
-    df = data_processor.df
-    
-    feature_engineer = (FeatureEngineer(df)
-        .process_floor()
-        .has_elevator_in_desc()
-        .has_balcony_in_desc()
-        .has_garage_in_desc()
-        .has_furniture_in_desc()
-        .create_luxury_col()
-        .create_room_per_area_col()
-        .reduce_to_binary('heating' , 'miejskie', 'district_heating')
-        .reduce_to_binary('ownership' , 'full_ownership', 'full_ownership')
-        .reduce_to_binary('market_type' , 'pierwotny', 'primary_market')
-        .reduce_building_type()
-        .frequency_encoding('district')
-        .one_hot_encode('finish_status')
-        .one_hot_encode('building_type')
-        .drop_columns(['description', 'ownership', 'heating', 'market_type', 'district'])
-        .scale_columns(['price', 'price_per_meter', 'area', 'year_built', 'rent', 'rooms', 'floor', 'building_max_floor'])
-        .save_to_csv("T")
-         )
+    X = df.drop('price', axis=1)
+    y = df['price']
 
-
-    feature_engineer.save(f"{PICKLE_DIR}/features_engineer_vT.pkl")
-    df = feature_engineer.df
-
-    print(df)
+    engineer = FeatureEngineer(config)
+    engineer.fit(X)
+    X = engineer.transform(X)
+    print(X)
